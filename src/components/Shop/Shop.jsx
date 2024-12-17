5080f5da2c3be6c4a55fbc39bdae72ded4dc160a
Nahid Hasan <nahidn228@gmail.com>
2024-12-17 22:35:53 +0600

    Fix unknown bug and code refactor

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -10,23 +10,34 @@ import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
+  const cart = useLoaderData();
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  //   const [cart, setCart] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [itemsPerPages, setItemsPerPages] = useState(10);
-  const { count } = useLoaderData();
+  const storedCart = getShoppingCart();
+  const storedCartIds = Object.keys(storedCart);
+  const [count, setCount] = useState(0);
+
+  //   const { count } = useLoaderData();
+  //   const count = 76;
   //   console.log(count);
-//   console.log(currentPage);
+  //   console.log(currentPage);
 
   const numberOfPages = Math.ceil(count / itemsPerPages);
 
+  const pages = [...Array(numberOfPages).keys()];
   //   const pages = [];
   //   for (let i = 0; i < numberOfPages; i++) {
   //     pages.push(i);
   //   }
   //   console.log(pages);
 
-  const pages = [...Array(numberOfPages).keys()];
+  useEffect(() => {
+    fetch("http://localhost:5000/productsCount")
+      .then((res) => res.json())
+      .then((data) => setCount(data.count));
+  }, []);
 
   useEffect(() => {
     fetch(
@@ -36,25 +47,24 @@ const Shop = () => {
       .then((data) => setProducts(data));
   }, [currentPage, itemsPerPages]);
 
-  useEffect(() => {
-    const storedCart = getShoppingCart();
-    const savedCart = [];
-    // step 1: get id of the addedProduct
-    for (const id in storedCart) {
-      // step 2: get product from products state by using id
-      const addedProduct = products.find((product) => product._id === id);
-      if (addedProduct) {
-        // step 3: add quantity
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        // step 4: add the added product to the saved cart
-        savedCart.push(addedProduct);
-      }
-      // console.log('added Product', addedProduct)
-    }
-    // step 5: set the cart
-    setCart(savedCart);
-  }, [products]);
+  //   useEffect(() => {
+  //     const savedCart = [];
+  //     // step 1: get id of the addedProduct
+  //     for (const id in storedCart) {
+  //       // step 2: get product from products state by using id
+  //       const addedProduct = products.find((product) => product._id === id);
+  //       if (addedProduct) {
+  //         // step 3: add quantity
+  //         const quantity = storedCart[id];
+  //         addedProduct.quantity = quantity;
+  //         // step 4: add the added product to the saved cart
+  //         savedCart.push(addedProduct);
+  //       }
+  //       // console.log('added Product', addedProduct)
+  //     }
+  //     // step 5: set the cart
+  //     setCart(savedCart);
+  //   }, [products]);
 
   const handleAddToCart = (product) => {
     // cart.push(product); '
